8bd69a59ab0fe14d9e7305e5a7bb94b570a9bb15
Andres Torres Sanchez <atorress@ucenfotec.ac.cr>
2024-06-21 21:46:30 -0600

    Ejercicio3
    Se agrega el ejercicio 3

--- Taarea1.cpp
+++ Taarea1.cpp
@@ -15,5 +15,31 @@ int main() {
     g.leerDatos(aristas);
     g.mostrarAdyacencias();
 
+    //Ejercicio 3
+    int numVertices = 6;
+
+    vector<vector<int>> adjacencyList(numVertices);
+
+    adjacencyList[0].push_back(4);
+    adjacencyList[0].push_back(5);
+    adjacencyList[1].push_back(4);
+    adjacencyList[1].push_back(5);
+    adjacencyList[1].push_back(6);
+    adjacencyList[2].push_back(4);
+    adjacencyList[2].push_back(5);
+    adjacencyList[2].push_back(6);
+    adjacencyList[3].push_back(4);
+    adjacencyList[3].push_back(5);
+    adjacencyList[3].push_back(6);
+
+    for (int i = 0; i < numVertices; i++) {
+        cout << "Vertex " << i + 1 << ": ";
+        for (int j = 0; j < adjacencyList[i].size(); j++) {
+            cout << adjacencyList[i][j] + 1 << " ";
+        }
+        cout << endl;
+    }
+
+
     return 0;
 }
